0dd4050574e023fe5569cac96eba56c7b0a92ff8
Jordan Cagle <54757419+caglej1@users.noreply.github.com>
2023-11-01 15:44:17 -0700

    Quiz questions now shuffle. Img at end of quiz.

--- Practice Projects/Quiz-App/src/components/Quiz.jsx
+++ Practice Projects/Quiz-App/src/components/Quiz.jsx
@@ -1,12 +1,26 @@
 import { useState } from "react";
 
 import QUESTIONS from "../questions.js";
+import quizCompleteImg from "../assets/quiz-complete.png";
 
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
 
   // Derive current question from userAnswers to manage as little state as possible.
   const activeQuestionIndex = userAnswers.length;
+  const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
+
+  if (quizIsComplete) {
+    return (
+      <div id="summary">
+        <img src={quizCompleteImg} alt="Trophy icon" />
+        <h2>Quiz Completed!</h2>
+      </div>
+    );
+  }
+
+  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+  shuffledAnswers.sort(() => Math.random() - 0.5); // 50/50 chance that questions are swapped.
 
   function handleSelectAnswer(selectedAnswer) {
     setUserAnswers((prevState) => {
@@ -19,7 +33,7 @@ export default function Quiz() {
       <div id="question">
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
-          {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+          {shuffledAnswers.map((answer) => (
             <li key={answer} className="answer">
               <button onClick={() => handleSelectAnswer(answer)}>
                 {answer}
