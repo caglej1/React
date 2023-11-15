1ea0f650ca562bbcbc70891d57be9b72064a33a4
Jordan Cagle <54757419+caglej1@users.noreply.github.com>
2023-11-15 15:16:05 -0800

    Demonstrating class-based component using redux.

--- Concepts/Redux-Intro/src/components/Counter.js
+++ Concepts/Redux-Intro/src/components/Counter.js
@@ -1,4 +1,4 @@
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import classes from "./Counter.module.css";
 
 const Counter = () => {
@@ -6,7 +6,6 @@ const Counter = () => {
   const counter = useSelector((state) => state.counter);
 
   const incrementHandler = () => {
-    console.log("Hello");
     dispatch({ type: "INCREMENT" });
   };
 
@@ -30,3 +29,51 @@ const Counter = () => {
 };
 
 export default Counter;
+
+// import { Component } from "react";
+// import { connect } from "react-redux";
+
+// class Counter extends Component {
+//   incrementHandler() {
+//     this.props.increment();
+//   }
+
+//   decrementHandler() {
+//     this.props.decrement();
+//   }
+
+//   toggleCounterHandler() {}
+
+//   render() {
+//     return (
+//       <main className={classes.counter}>
+//         <h1>Redux Counter</h1>
+//         <div className={classes.value}>{this.props.counter}</div>
+//         <div>
+//           <button onClick={this.incrementHandler.bind(this)}>Increment</button>
+//           <button onClick={this.decrementHandler.bind(this)}>Decrement</button>
+//         </div>
+//         <button onClick={this.toggleCounterHandler}>Toggle Counter</button>
+//       </main>
+//     );
+//   }
+// }
+
+// const mapStateToProps = (state) => {
+//   return {
+//     counter: state.counter,
+//   };
+// };
+
+// const mapDispatchToProps = (dispatch) => {
+//   return {
+//     increment: () => dispatch({ type: "INCREMENT" }),
+//     decrement: () => dispatch({ type: "DECREMENT" }),
+//   };
+// };
+
+// For class-based components, use connect from react-redux.
+// connect takes 2 functions as arguments, which are essentially the equivalent of useSelector and useDispatch.
+// connect returns a function when executed and we pass our Counter class-based component to that returned function as an argument.
+// The returned function is also executed.
+// export default connect(mapStateToProps, mapDispatchToProps)(Counter);
