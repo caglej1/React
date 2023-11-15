af503073baf59109883ca0b4abee5cf6799c77b3
Jordan Cagle <54757419+caglej1@users.noreply.github.com>
2023-11-15 15:29:41 -0800

    Demonstrating working with multiple state properties.

--- Concepts/Redux-Intro/src/store/index.js
+++ Concepts/Redux-Intro/src/store/index.js
@@ -1,21 +1,35 @@
 import { createStore } from "redux";
 
-const counterReducer = (state = { counter: 0 }, action) => {
+const initialState = { counter: 0, showCounter: true };
+
+// Note: The redux reducer function does not merge state changes!
+// It replaces the state with what is returned. Keep this in mind.
+const counterReducer = (state = initialState, action) => {
   if (action.type === "INCREMENT") {
     return {
       counter: state.counter + 1,
+      showCounter: state.showCounter,
     };
   }
 
   if (action.type === "INCREASE") {
     return {
       counter: state.counter + action.amount,
+      showCounter: state.showCounter,
     };
   }
 
   if (action.type === "DECREMENT") {
     return {
       counter: state.counter - 1,
+      showCounter: state.showCounter,
+    };
+  }
+
+  if (action.type === "TOGGLE") {
+    return {
+      showCounter: !state.showCounter,
+      counter: state.counter,
     };
   }
 
