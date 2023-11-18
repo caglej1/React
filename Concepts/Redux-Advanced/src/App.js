7b142f5240390a0846094ec982a904d167d5b852
Jordan Cagle <54757419+caglej1@users.noreply.github.com>
2023-11-18 15:09:56 -0800

    Using Action Creator Thunk to achieve the same result as when the HTTP state logic is in the App component.

--- Concepts/Redux-Advanced/src/App.js
+++ Concepts/Redux-Advanced/src/App.js
@@ -3,8 +3,8 @@ import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 import { useEffect } from "react";
-import { uiActions } from "./store/ui-slice";
 import Notification from "./components/UI/Notification";
+import { sendCartData } from "./store/cart-slice";
 
 let isInitial = true;
 
@@ -15,50 +15,13 @@ function App() {
   const notification = useSelector((state) => state.ui.notification);
 
   useEffect(() => {
-    const sendCartData = async () => {
-      dispatch(
-        uiActions.showNotification({
-          status: "pending",
-          title: "Sending...",
-          message: "Sending cart data!",
-        })
-      );
-
-      const response = await fetch(
-        "https://react-redux-async-demo-a1edc-default-rtdb.firebaseio.com/cart.json",
-        {
-          method: "PUT",
-          body: JSON.stringify(cart),
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error("Sending cart data failed.");
-      }
-
-      dispatch(
-        uiActions.showNotification({
-          status: "success",
-          title: "Success!",
-          message: "Sent cart data successfully!",
-        })
-      );
-    };
-
     if (isInitial) {
       isInitial = false;
       return;
     }
 
-    sendCartData().catch(() => {
-      dispatch(
-        uiActions.showNotification({
-          status: "error",
-          title: "Error!",
-          message: "Sending cart data failed!",
-        })
-      );
-    });
+    // Redux toolkit accepts custom action creator thunk. It will execute the function returned by the thunk and pass dispatch() to it.
+    dispatch(sendCartData(cart));
   }, [cart, dispatch]);
   // react-redux ensures that the dispatch function never changes, thus it is safe to add to the dependency list.
 
