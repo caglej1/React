cd1d12015d16fa2ff4484304b747ede3866f4eba
Jordan Cagle <54757419+caglej1@users.noreply.github.com>
2023-11-17 17:02:04 -0800

    Using useEffect with Redux.
    useSelector subscribes to the redux store, thus whenever state.cart is updated, useEffect will execute and send a PUT request with the cart data to the backend.
    
    The cart data will have been updated through the redux reducer function, therefore we are sending the formatted cart data to the backend.

--- Concepts/Redux-Advanced/src/App.js
+++ Concepts/Redux-Advanced/src/App.js
@@ -2,9 +2,21 @@ import { useSelector } from "react-redux";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
+import { useEffect } from "react";
 
 function App() {
   const cartIsVisible = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    fetch(
+      "https://react-redux-async-demo-a1edc-default-rtdb.firebaseio.com/cart.json",
+      {
+        method: "PUT",
+        body: JSON.stringify(cart),
+      }
+    );
+  }, [cart]);
 
   return (
     <Layout>
