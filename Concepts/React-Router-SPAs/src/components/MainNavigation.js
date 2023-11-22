2531da21d09cd511d51c1039999c84d70afad2f6
Jordan Cagle <54757419+caglej1@users.noreply.github.com>
2023-11-21 16:21:28 -0800

    Showing errorElement + using NavLink component.

--- Concepts/React-Router-SPAs/src/components/MainNavigation.js
+++ Concepts/React-Router-SPAs/src/components/MainNavigation.js
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import classes from "./MainNavigation.module.css";
 
 export default function MainNavigation() {
@@ -7,10 +7,25 @@ export default function MainNavigation() {
       <nav>
         <ul className={classes.list}>
           <li>
-            <Link to="/">Home</Link>
+            <NavLink
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+              to="/"
+              end
+            >
+              Home
+            </NavLink>
           </li>
           <li>
-            <Link to="/products">Products</Link>
+            <NavLink
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+              to="/products"
+            >
+              Products
+            </NavLink>
           </li>
         </ul>
       </nav>
