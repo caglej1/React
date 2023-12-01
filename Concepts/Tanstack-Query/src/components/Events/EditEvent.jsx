8881d79bc476053fb4e2c3b7d9f7a6bcdbdd9aca
Jordan Cagle <54757419+caglej1@users.noreply.github.com>
2023-11-30 16:36:42 -0800

    Using both tanstack query and react router.
    Demonstrating how elements of react router (loaders + actions) may be used in conjunction with element of tanstack query to achieve the same goal.
    
    It is a matter of preference which approach to use.

--- Concepts/Tanstack-Query/src/components/Events/EditEvent.jsx
+++ Concepts/Tanstack-Query/src/components/Events/EditEvent.jsx
@@ -1,52 +1,62 @@
-import { Link, useNavigate, useParams } from "react-router-dom";
+import {
+  Link,
+  redirect,
+  useNavigate,
+  useParams,
+  useSubmit,
+  useNavigation,
+} from "react-router-dom";
 
 import Modal from "../UI/Modal.jsx";
 import EventForm from "./EventForm.jsx";
-import { useMutation, useQuery } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import { fetchEvent, queryClient, updateEvent } from "../../util/http.js";
-import LoadingIndicator from "../UI/LoadingIndicator.jsx";
 import ErrorBlock from "../UI/ErrorBlock.jsx";
 
 export default function EditEvent() {
   const navigate = useNavigate();
+  const { state } = useNavigation();
   const params = useParams();
 
-  const { data, isPending, isError, error } = useQuery({
+  const submit = useSubmit();
+
+  const { data, isError, error } = useQuery({
     queryKey: ["events", params.id],
     queryFn: ({ signal }) => fetchEvent({ id: params.id, signal }),
+    staleTime: 10000, // Loader fetches data immediately before this call, so prevent immediate re-fetch.
   });
 
-  const { mutate } = useMutation({
-    mutationFn: updateEvent,
-    onMutate: async (data) => {
-      // data is forwarded to this function from the mutationFn.
-      const newEvent = data.event;
+  // const { mutate } = useMutation({
+  //   mutationFn: updateEvent,
+  //   onMutate: async (data) => {
+  //     // data is forwarded to this function from the mutationFn.
+  //     const newEvent = data.event;
 
-      // Cancel any queries with same queryKey to avoid clashing response data.
-      await queryClient.cancelQueries({ queryKey: ["events", params.id] });
+  //     // Cancel any queries with same queryKey to avoid clashing response data.
+  //     await queryClient.cancelQueries({ queryKey: ["events", params.id] });
 
-      // Store old data in case query fails so we can revert.
-      const previousEvent = queryClient.getQueryData(["events", params.id]);
+  //     // Store old data in case query fails so we can revert.
+  //     const previousEvent = queryClient.getQueryData(["events", params.id]);
 
-      queryClient.setQueryData(["events", params.id], newEvent);
+  //     queryClient.setQueryData(["events", params.id], newEvent);
 
-      // This returned object will be the context argument in the onError function below.
-      return { previousEvent };
-    },
-    onError: (error, data, context) => {
-      // Roll back optimistic update in case an error occurs.
-      queryClient.setQueryData(["events", params.id], context.previousEvent);
-    },
-    onSettled: () => {
-      // onSettled is called once the mutation is done, regardless of if it succeeded or failed.
-      // To ensure all front-end data matches back-end data (data is in sync), invalidate the query.
-      queryClient.invalidateQueries(["events", params.id]);
-    },
-  });
+  //     // This returned object will be the context argument in the onError function below.
+  //     return { previousEvent };
+  //   },
+  //   onError: (error, data, context) => {
+  //     // Roll back optimistic update in case an error occurs.
+  //     queryClient.setQueryData(["events", params.id], context.previousEvent);
+  //   },
+  //   onSettled: () => {
+  //     // onSettled is called once the mutation is done, regardless of if it succeeded or failed.
+  //     // To ensure all front-end data matches back-end data (data is in sync), invalidate the query.
+  //     queryClient.invalidateQueries(["events", params.id]);
+  //   },
+  // });
 
   function handleSubmit(formData) {
-    mutate({ id: params.id, event: formData });
-    navigate("../");
+    // Trigger action function.
+    submit(formData, { method: "PUT" });
   }
 
   function handleClose() {
@@ -54,14 +64,6 @@ export default function EditEvent() {
   }
 
   let content;
-
-  if (isPending) {
-    content = (
-      <div className="center">
-        <LoadingIndicator />
-      </div>
-    );
-  }
 
   if (isError) {
     content = (
@@ -85,15 +87,41 @@ export default function EditEvent() {
   if (data) {
     content = (
       <EventForm inputData={data} onSubmit={handleSubmit}>
-        <Link to="../" className="button-text">
-          Cancel
-        </Link>
-        <button type="submit" className="button">
-          Update
-        </button>
+        {state === "submitting" ? (
+          <p>Sending data...</p>
+        ) : (
+          <>
+            <Link to="../" className="button-text">
+              Cancel
+            </Link>
+            <button type="submit" className="button">
+              Update
+            </button>
+          </>
+        )}
       </EventForm>
     );
   }
 
   return <Modal onClose={handleClose}>{content}</Modal>;
+}
+
+export function loader({ params }) {
+  // Programmatically send a query without needing a component execution.
+  // When useQuery is called with the below queryKey, the data will already be fetched,
+  // thus useQuery will get this data from the cache.
+  return queryClient.fetchQuery({
+    queryKey: ["events", params.id],
+    queryFn: ({ signal }) => fetchEvent({ id: params.id, signal }),
+  });
+}
+
+export async function action({ request, params }) {
+  const formData = await request.formData();
+  const updatedEventData = Object.fromEntries(formData);
+
+  await updateEvent({ id: params.id, event: updatedEventData });
+  await queryClient.invalidateQueries(["events"]);
+
+  return redirect("../");
 }
