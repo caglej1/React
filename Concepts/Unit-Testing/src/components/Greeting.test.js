06f04fd5a32043a701502e9649e37c0d6c26ec82
Jordan Cagle <54757419+caglej1@users.noreply.github.com>
2023-12-05 14:31:51 -0800

    Testing user interaction and state.

--- Concepts/Unit-Testing/src/components/Greeting.test.js
+++ Concepts/Unit-Testing/src/components/Greeting.test.js
@@ -1,14 +1,54 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Greeting from "./Greeting";
 
-test("renders Hello World as text", () => {
-  // Arrange
-  render(<Greeting />);
+describe("Greeting component", () => {
+  test("renders Hello World as text", () => {
+    // Arrange
+    render(<Greeting />);
 
-  // Act
-  // ... Nothing in this case.
+    // Act
+    // ... Nothing in this case.
 
-  // Assert
-  const hellowWorldElement = screen.getByText("Hello World", { exact: false });
-  expect(hellowWorldElement).toBeInTheDocument();
+    // Assert
+    const hellowWorldElement = screen.getByText("Hello World", {
+      exact: false,
+    });
+    expect(hellowWorldElement).toBeInTheDocument();
+  });
+
+  test("renders good to see you as text if the button was NOT clicked", () => {
+    render(<Greeting />);
+
+    const outputElement = screen.getByText("good to see you", { exact: false });
+    expect(outputElement).toBeInTheDocument();
+  });
+
+  test("renders 'Changed!' if the button was clicked", () => {
+    // Arrange
+    render(<Greeting />);
+
+    // Act
+    const buttonElement = screen.getByRole("button");
+    userEvent.click(buttonElement);
+
+    // Assert
+    const outputElement = screen.getByText("changed", { exact: false });
+    expect(outputElement).toBeInTheDocument();
+  });
+
+  test("does not render 'good to see you' if the button was clicked", () => {
+    // Arrange
+    render(<Greeting />);
+
+    // Act
+    const buttonElement = screen.getByRole("button");
+    userEvent.click(buttonElement);
+
+    // Assert
+    const outputElement = screen.queryByText("good to see you", {
+      exact: false,
+    });
+    expect(outputElement).toBeNull();
+  });
 });
